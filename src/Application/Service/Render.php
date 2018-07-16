63bf271d6977a27e37e649b8a6adc68ac1c69ad7
aureliored <aureliomoreirared@gmail.com>
2018-07-16 01:09:23 -0300

    Feature: Serviço de controle de renderização de template

---
+++ src/Application/Service/Render.php
@@ -0,0 +1,66 @@
+<?php
+namespace Application\Service;
+
+class Render 
+{
+    private $path;
+    private $base;
+    private $content;
+
+    public function __construct()
+    {
+        $this->path = __TEMPLATE_PATH__;
+        $this->base = __TEMPLATE_DEFAULT__;
+    }
+
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+    
+    public function getBase()
+    {
+        return $this->base;
+    }
+
+    public function setBase($base)
+    {
+        $this->base = $base;
+    }
+
+    public function getContent($render, $module, $data)
+    {
+        $src = __APP_ROOT__ . '\\src\\' . $module . "\\View\\" . $render . '.php';
+        ob_start();
+        include $src;
+        $this->content =  ob_get_clean();
+        return $this->content;
+    }
+
+    public function makeRender($template)
+    {
+        return str_replace('{{content}}',$this->content, $template);
+    }
+
+    function getTemplate(){
+        $title = 'Teste';
+		ob_start();
+        include $this->path . '\\' . $this->base;
+        $html =  ob_get_clean();
+        return $html;
+    }
+    
+    public function render($render, $module, $data = [],  $base = null)
+    {
+        $this->getContent($render, $module,$data);
+        
+        $template = $this->getTemplate();
+        echo $this->makeRender($template);
+        return $this;
+    }
+}
